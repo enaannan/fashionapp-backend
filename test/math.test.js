4c482e94257f972b644a2ff772abc36fdba20706
Emmanuel Annan <enaannan@gmail.com>
2020-06-30 08:39:25 +0100

    one test failed

--- test/math.test.js
+++ test/math.test.js
@@ -5,7 +5,7 @@ describe('math.js tests', () => {
     describe('math.add() Test', () => {
         it('should equal 2', () => {
             const result = math.add(1, 1);
-            assert.equal(result, 2);
+            assert.equal(result, 4);
         });
         it('should equal 4', () => {
             const result = math.add(2, 2);
